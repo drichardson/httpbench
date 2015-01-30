6f9af343ba4d55acb388f7a037e35610c5c2c220
Doug Richardson <dougie.richardson@gmail.com>
2015-01-29 22:08:42 -0800

    go ideal

--- servers/go/ideal.go
+++ servers/go/ideal.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
+	"time"
 )
 
 var buf string
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	//fmt.Println("Handling")
+	//time.Sleep(5 * time.Second)
 	w.Header().Set("ContentType", "text/plain")
 	w.Header().Set("ContentLength", "1024")
 	w.Write([]byte(buf))
+	//fmt.Println("Done Handling")
 }
 
 func main() {
